35a3ed429662224ab7b5e0f28c8a35e1692592b8
Venera <vgeorgievaaaa@gmail.com>
2024-06-27 21:23:26 +0300

    add warnings

--- Components - Basic Idea/components-basics/src/components/Counter.jsx
+++ Components - Basic Idea/components-basics/src/components/Counter.jsx
@@ -10,9 +10,23 @@ export default function Counter(props) {
     setCount(0);
   };
 
+//   if (count < 0) {
+//     return (
+// <h3>Invalid count!</h3>
+// );
+//   }
+
   return (
     <div>
       <h1>Counter</h1>
+
+       {count < 0 
+        ? <p>Invalid count!</p>
+        : <p>Valid count!</p>   
+    }
+
+    {count == 0 && <p>Please start incrementing</p> }
+
       <p>Count: {count}</p>
       <button onClick={() => setCount(count - 1)}>-</button>
       <button onClick={clearCounterHandler}>clear</button>
