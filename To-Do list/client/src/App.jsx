e779f8ed424317c23068d321430296f7d7b06e39
Venera <vgeorgievaaaa@gmail.com>
2024-07-03 11:39:38 +0300

    add components,add functionality

--- To-Do list/client/src/App.jsx
+++ To-Do list/client/src/App.jsx
@@ -1,8 +1,19 @@
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
+import TodoList from "./components/TodoList.jsx";
 function App() {
   return(
     <div>
-      
-    </div>
+ <Header />
+  <main className="main">
+
+ <TodoList />
+  </main>
+
+
+<Footer />
+   
+   </div>
   )
 }
 
---
+++ To-Do list/client/src/components/TodoList.jsx
@@ -0,0 +1,55 @@
+import { useEffect,useState } from "react"
+import TodoItem from "./TodoItem.jsx"
+export default function TodoList(){
+     const [todos,setTodos] = useState([]);
+
+    useEffect(()=>{
+        fetch('http://localhost:3030/jsonstore/todos')
+        .then(response => response.json())
+        .then(data =>{
+            
+            setTodos(Object.values(data))
+        })
+        .catch(err => console.log(err))
+    },[])
+
+    const changeStatusHandler = (todoId) => {
+      setTodos(state => state.map(todo => todo._id === todoId ? {...todo,isCompleted: !todo.isCompleted} : todo))
+    }
+    return (
+
+    <section className="todo-list-container">
+    <h1>Todo List</h1>
+
+    <div className="add-btn-container">
+      <button className="btn">+ Add new Todo</button>
+    </div>
+
+    <div className="table-wrapper">
+
+      <table className="table">
+        <thead>
+          <tr>
+            <th className="table-header-task">Task</th>
+            <th className="table-header-status">Status</th>
+            <th className="table-header-action">Action</th>
+          </tr>
+        </thead>
+        <tbody>
+
+            {todos.map(todo=>(
+                   <TodoItem
+                   key={todo._id}
+                   _id={todo._id}
+                   text={todo.text}
+                   isCompleted ={todo.isCompleted}
+                   changeStatusHandler={changeStatusHandler}
+                   />
+  
+            ))}
+        </tbody>
+      </table>
+    </div>
+  </section>
+    )
+}
