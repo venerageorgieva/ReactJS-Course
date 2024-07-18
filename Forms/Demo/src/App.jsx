bfff0adb645cf85f212714b9a7c0a2a179c102ac
Venera <vgeorgievaaaa@gmail.com>
2024-07-18 20:58:06 +0300

    useRef() demo

--- Forms/Demo/src/App.jsx
+++ Forms/Demo/src/App.jsx
@@ -1,12 +1,20 @@
 import UncontrolledForm from './components/UncontrolledForm.jsx'
 import ControlledForm from './components/ControlledForm.jsx'
 import './App.css'
+import { useRef } from 'react'
 
 function App() {
-
+const formRef = useRef();
   return (
     <>
- <ControlledForm />
+ <ControlledForm formRef={formRef}/>
+
+ <button
+  type='button' 
+  onClick={()=> formRef.current.requestSubmit()}
+  >
+   Submit
+ </button>
  </>
   )
 }
--- Forms/Demo/src/components/ControlledForm.jsx
+++ Forms/Demo/src/components/ControlledForm.jsx
@@ -5,31 +5,29 @@ const formInitialState = {
   password: "",
   age: "",
   gender: "f",
-  swimming:false,
-  shopping:false,
-  running: false
+  swimming: false,
+  shopping: false,
+  running: false,
 };
 
-export default function ControlledForm() {
+export default function ControlledForm(formRef) {
   const [formValues, setFormValues] = useState(formInitialState);
 
   const changeHandler = (e) => {
-    let value = '';
+    let value = "";
 
+    switch (e.target.type) {
+      case "number":
+        value = Number(e.target.value);
+        break;
+      case "checkbox":
+        value = e.target.checked;
+        break;
+      default:
+        value = e.target.value;
+        break;
+    }
 
-
-        switch(e.target.type){
-            case 'number':
-                value = Number(e.target.value);
-                break;
-                case 'checkbox':
-                    value = e.target.checked;
-                    break;
-                default:
-                    value = e.target.value;
-                    break;
-        }
-    
     setFormValues((state) => ({
       ...state,
       [e.target.name]: e.target,
@@ -45,15 +43,15 @@ export default function ControlledForm() {
   };
 
   const submitHandler = () => {
+    e.preventDefault();
     console.log(formValues);
     resetFormHandler();
   };
 
-
   return (
     <>
       <h1>Controlled Form</h1>
-      <form>
+      <form ref={formRef} onSubmit={submitHandler}>
         <div>
           <label htmlFor='username'>Username:</label>
           <input
@@ -87,20 +85,42 @@ export default function ControlledForm() {
         </div>
         <div>
           <label htmlFor='gender'>Gender</label>
-          <select name='gender' id='gender' onChange={changeHandler} value={formValues.gender}>
+          <select
+            name='gender'
+            id='gender'
+            onChange={changeHandler}
+            value={formValues.gender}
+          >
             <option value='f'>F</option>
             <option value='m'>M</option>
           </select>
         </div>
         <div>
-            <h3>Hobbies</h3>
-            <label htmlFor="swimming">Swimming</label>
-            <input type="checkbox" name="swimming" id="swimming" checked={formValues.swimming} onChange={changeHandler}/>
-            <label htmlFor="shopping">Shopping</label>
-            <input type="checkbox" name="shopping" id="shopping" checked={formValues.shopping} onChange={changeHandler}/>
-            <label htmlFor="running">Running</label>
-            <input type="checkbox" name="running" id="running" checked={formValues.running} onChange={changeHandler}/>
-
+          <h3>Hobbies</h3>
+          <label htmlFor='swimming'>Swimming</label>
+          <input
+            type='checkbox'
+            name='swimming'
+            id='swimming'
+            checked={formValues.swimming}
+            onChange={changeHandler}
+          />
+          <label htmlFor='shopping'>Shopping</label>
+          <input
+            type='checkbox'
+            name='shopping'
+            id='shopping'
+            checked={formValues.shopping}
+            onChange={changeHandler}
+          />
+          <label htmlFor='running'>Running</label>
+          <input
+            type='checkbox'
+            name='running'
+            id='running'
+            checked={formValues.running}
+            onChange={changeHandler}
+          />
         </div>
         <div>
           <button type='button' onClick={submitHandler}>
