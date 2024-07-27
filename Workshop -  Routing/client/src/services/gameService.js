bfccd6a91c722c41792fd034d3843a9f0b2734ea
Venera <vgeorgievaaaa@gmail.com>
2024-07-27 19:54:44 +0300

    added get all games service method

--- Workshop -  Routing/client/src/services/gameService.js
+++ Workshop -  Routing/client/src/services/gameService.js
@@ -1,7 +1,15 @@
-const baseUrl = "http://localhost:3030/jsonstore";
+import { request } from "../lib/request.js";
+const baseUrl = "http://localhost:3030/jsonstore/games";
+
+
+export const getAll = async () => {
+  const result = await request('GET',baseUrl);
+  console.log(result);
+  return Object.values(result);
+};
 
 export const create = async (gameData) => {
-  const response = await fetch(`${baseUrl}/games`, {
+  const response = await fetch(baseUrl, {
     method: "POST",
     headers: {
       "content-type": "application/json",
