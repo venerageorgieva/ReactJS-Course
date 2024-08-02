b589dd90b400afce0dd127fa52cf9ae6d1d27691
Venera <vgeorgievaaaa@gmail.com>
2024-08-02 17:16:55 +0300

    change login to controlled form

--- Workshop -  Routing/client/src/components/login/Login.jsx
+++ Workshop -  Routing/client/src/components/login/Login.jsx
@@ -1,22 +1,41 @@
-export default function Login(){
-    return(
-        <section id="login-page" className="auth">
-        <form id="login">
+import useForm from "../../hooks/useForm.js";
+export default function Login() {
+  const { values, onChange, onSubmit } = useForm({
+    email: "",
+    password: "",
+  });
+  return (
+    <section id='login-page' className='auth'>
+      <form id='login' onSubmit={onSubmit}>
+        <div className='container'>
+          <div className='brand-logo'></div>
+          <h1>Login</h1>
+          <label htmlFor='email'>Email:</label>
+          <input
+            type='email'
+            id='email'
+            name='email'
+            placeholder='Sokka@gmail.com'
+            onChange={onChange}
+            value={values.email}
+          />
 
-            <div className="container">
-                <div className="brand-logo"></div>
-                <h1>Login</h1>
-                <label htmlFor="email">Email:</label>
-                <input type="email" id="email" name="email" placeholder="Sokka@gmail.com" />
-
-                <label htmlFor="login-pass">Password:</label>
-                <input type="password" id="login-password" name="password" />
-                <input type="submit" className="btn submit" value="Login" />
-                <p className="field">
-                    <span>If you don't have profile click <a href="#">here</a></span>
-                </p>
-            </div>
-        </form>
+          <label htmlFor='login-pass'>Password:</label>
+          <input
+            type='password'
+            id='login-password'
+            name='password'
+            onChange={onChange}
+            value={values.password}
+          />
+          <input type='submit' className='btn submit' value='Login' />
+          <p className='field'>
+            <span>
+              If you don't have profile click <a href='#'>here</a>
+            </span>
+          </p>
+        </div>
+      </form>
     </section>
-    )
+  );
 }
