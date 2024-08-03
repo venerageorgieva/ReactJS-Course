8e59538d6149d3c86870416c9089930307b115b3
Venera <vgeorgievaaaa@gmail.com>
2024-08-03 19:00:58 +0300

    add user register

--- Workshop -  Routing/client/src/App.jsx
+++ Workshop -  Routing/client/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import { Routes, Route,useNavigate } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 
-import * as authService from './services/authService.js'
+import * as authService from "./services/authService.js";
 import AuthContext from "./contexts/authContext.js";
 import Path from "./paths.js";
 
@@ -14,42 +14,45 @@ import Register from "./components/register/Register.jsx";
 import GameDetails from "./components/game-details/GameDetails.jsx";
 function App() {
   const navigate = useNavigate();
-  const [auth,setAuth]= useState({});
+  const [auth, setAuth] = useState({});
 
+  const loginSubmitHandler = async (values) => {
+    const result = await authService.login(values.email, values.password);
 
-  const loginSubmitHandler = async (values) =>{
-   const result = await authService.login(values.email,values.password)
-   
-   setAuth(result);
+    setAuth(result);
 
-   navigate(Path.Home)
-  }
+    navigate(Path.Home);
+  };
 
   const registerSubmitHandler = async (values) => {
-    console.log(values);
-  }
+    const result = await authService.register(values.email, values.password);
+
+    setAuth(result);
+
+    navigate(Path.Home);
+  };
 
   const values = {
     loginSubmitHandler,
     registerSubmitHandler,
-    username:auth.username,
-    email:auth.email,
-    isAuthenticated: !!auth.username,
-  }
+    username: auth.username || auth.email,
+    email: auth.email,
+    isAuthenticated: !!auth.email,
+  };
 
   return (
-   < AuthContext.Provider value={values}>
-    <div id='box'>
-      <Header />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/games' element={<GameList />} />
-        <Route path="/games/create" element={<GameCreate />}/>
-        <Route path="/login" element={<Login />}/>
-        <Route path="/register" element={<Register />}/>
-        <Route path="/games/:gameId" element={<GameDetails />} />
- ƒ     </Routes>
-    </div>
+    <AuthContext.Provider value={values}>
+      <div id='box'>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/games' element={<GameList />} />
+          <Route path='/games/create' element={<GameCreate />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/games/:gameId' element={<GameDetails />} />ƒ{" "}
+        </Routes>
+      </div>
     </AuthContext.Provider>
   );
 }
--- Workshop -  Routing/client/src/services/authService.js
+++ Workshop -  Routing/client/src/services/authService.js
@@ -1,12 +1,18 @@
 import * as request from "../lib/request.js";
 
-const baseUrl = 'http://localhost:3030/users'
+const baseUrl = "http://localhost:3030/users";
 
-export const login = async (email,password) =>{
-  const result = await request.post(`${baseUrl}/login`,{
+export const login = async (email, password) => {
+  const result = await request.post(`${baseUrl}/login`, {
     email,
-    password
+    password,
   });
 
   return result;
-}
+};
+
+export const register = async (email, password) =>
+  request.post(`${baseUrl}/register`, {
+    email,
+    password,
+  });
