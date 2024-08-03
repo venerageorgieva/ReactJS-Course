4fb031d0c385febac076414fd20b27fb350ff7bc
Venera <vgeorgievaaaa@gmail.com>
2024-08-03 18:49:50 +0300

    change register form to controlled

--- Workshop -  Routing/client/src/components/register/Register.jsx
+++ Workshop -  Routing/client/src/components/register/Register.jsx
@@ -1,27 +1,67 @@
-export default function Register(){
-    return(
-        <section id="register-page" className="content auth">
-        <form id="register">
-            <div className="container">
-                <div className="brand-logo"></div>
-                <h1>Register</h1>
+import { useContext } from "react";
+import AuthContext from "../../contexts/authContext.js";
+import useForm from "../../hooks/useForm.js";
 
-                <label htmlFor="email">Email:</label>
-                <input type="email" id="email" name="email" placeholder="maria@email.com" />
+const registerFormKeys = {
+  Email: "email",
+  Password: "password",
+  ConfirmPassword: "confirm-password",
+};
 
-                <label htmlFor="pass">Password:</label>
-                <input type="password" name="password" id="register-password" />
+export default function Register() {
+  const { registerSubmitHandler } = useContext(AuthContext);
+  const { values, onChange, onSubmit } = useForm(registerSubmitHandler, {
+    [registerFormKeys.Email]: "",
+    [registerFormKeys.Password]: "",
+    [registerFormKeys.ConfirmPassword]: "",
+  });
 
-                <label htmlFor="con-pass">Confirm Password:</label>
-                <input type="password" name="confirm-password" id="confirm-password" />
+  return (
+    <section id='register-page' className='content auth'>
+      <form id='register' onSubmit={onSubmit}>
+        <div className='container'>
+          <div className='brand-logo'></div>
+          <h1>Register</h1>
 
-                <input className="btn submit" type="submit" value="Register" />
+          <label htmlFor='email'>Email:</label>
+          <input
+            type='email'
+            id='email'
+            name='email'
+            placeholder='maria@email.com'
+            onChange={onChange}
+            values={values[registerFormKeys.Email]}
+          />
 
-                <p className="field">
-                    <span>If you already have profile click <a href="#">here</a></span>
-                </p>
-            </div>
-        </form>
+          <label htmlFor='pass'>Password:</label>
+          <input
+            type='password'
+            name='password'
+            id='register-password'
+            onChange={onChange}
+            values={values[registerFormKeys.Password]}
+
+          />
+
+          <label htmlFor='con-pass'>Confirm Password:</label>
+          <input
+            type='password'
+            name='confirm-password'
+            id='confirm-password'
+            onChange={onChange}
+            values={values[registerFormKeys.ConfirmPassword]}
+
+          />
+
+          <input className='btn submit' type='submit' value='Register' />
+
+          <p className='field'>
+            <span>
+              If you already have profile click <a href='#'>here</a>
+            </span>
+          </p>
+        </div>
+      </form>
     </section>
-    )
+  );
 }
